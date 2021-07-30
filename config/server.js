6e3793130eb3fa976a11c9c6ad4681225fe7faee
IdoShmida <shmidamail@gmail.com>
2021-07-30 21:31:38 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b079a9fdc607104fa424ae707230cd4d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '02bff75e-9951-4d63-99a2-acecf0134d27'
+};
