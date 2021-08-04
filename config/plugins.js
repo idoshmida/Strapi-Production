d72ff6681789852ce16c02cf962b7eccf77eb5fb
IdoShmida <shmidamail@gmail.com>
2021-08-04 20:25:17 +0300

    commit to chek the s3 before env into heroku

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,13 @@
-module.exports = ({ env }) => {
-
-    if(env('NODE_ENV') === 'production'){
-        return {
-            upload: {
-                provider: 'aws-s3',
-                providerOptions: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                        Bucket: env('AWS_BUCKET'),
-                    },
-                },
-            },
-        }
-    }
-
-    return {
-        //Empty config which will use defaults
-    }
-}
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: 'us-east-1',
+        params: {
+          Bucket: 'strapi-plants-provider',
+        },
+      },
+    },
+  });
